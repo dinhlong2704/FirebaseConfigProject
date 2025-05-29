31553a7fa26f1a6b9709d25229332cd958082b46
RUNSYSTEM\longdp <longdp@runsystem.net>
2025-05-29 18:00:59 +0700

    FirebaseConfigProject

--- app/src/main/java/com/example/firebaseconfigproject/MainActivity.kt
+++ app/src/main/java/com/example/firebaseconfigproject/MainActivity.kt
@@ -1,47 +1,39 @@
 package com.example.firebaseconfigproject
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.firebaseconfigproject.ui.theme.FirebaseConfigProjectTheme
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.firebaseconfigproject.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.remoteConfig
+import com.google.firebase.remoteconfig.remoteConfigSettings
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var remoteConfig: FirebaseRemoteConfig
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            FirebaseConfigProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        remoteConfig = Firebase.remoteConfig
+        remoteConfig.setConfigSettingsAsync(
+            remoteConfigSettings {
+                minimumFetchIntervalInSeconds = 0
             }
+        )
+
+        remoteConfig.setDefaultsAsync(
+            mapOf("welcome_message" to "Chào từ default config")
+        )
+
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            val msg = remoteConfig.getString("welcome_message")
+            findViewById<TextView>(R.id.tv_appname).text = msg
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirebaseConfigProjectTheme {
-        Greeting("Android")
-    }
-}
